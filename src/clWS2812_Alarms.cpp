da8dd562b5b4a3352e020ca48dad6046527c26db
Oleh Baranov <obara1962@gmail.com>
2024-11-24 12:27:42 +0200

    first3

--- src/clWS2812_Alarms.cpp
+++ src/clWS2812_Alarms.cpp
@@ -3,7 +3,7 @@
 clWS2812_Alarm::clWS2812_Alarm(){
     for(int i = 0; i < 2; i++){
         alarm[i].setPixelNumber(i + 70);
-        alarm[i].setHue(0);
+        alarm[i].setHue(3);
         alarm[i].setSaturation(0);
         alarm[i].setValue(0);
     } 
